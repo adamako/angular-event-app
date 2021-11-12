23bb2850728c0d65f2e3a3cf43cd48d9aa7e7a64
Adama KO <adamako34@hotmail.com>
2021-11-12 11:57:22 +0000

    TASK: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello world</h1>
+    <img src="/assets/images/angular-logo.png" alt="logo" /> `,
 })
 export class AppComponent {
   title = 'event-app';
---
+++ src/app/events/event-list/events-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.css'],
+})
+export class EventsListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
