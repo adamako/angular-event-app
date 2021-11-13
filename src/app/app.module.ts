a449edd12ae9701ecdfd06d501df850bc0952c90
Adama KO <adamako34@hotmail.com>
2021-11-13 08:51:32 +0000

    TASK: add dependency injection service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/event-list/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 import { NavComponent } from './nav/nav.component';
+import { EventService } from './services/event.service';
 
 @NgModule({
-  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavComponent,
+  ],
   imports: [BrowserModule],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
